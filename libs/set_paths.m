f8f789a93d0121657faecd75e0843e19e9f31ca1
Anjan Dutta <duttanjan@gmail.com>
2018-01-04 18:04:27 +0100

    mistakenly committed set_paths.m

--- libs/set_paths.m
+++ libs/set_paths.m
@@ -2,8 +2,8 @@ function [ libpaths ] = set_paths()
 
     % Set path accordingly for each computer
 
-    libpaths.matlabbgl = '/home/adutta/Dropbox/Personal/Workspace/AdditionalTools/' ;
-    libpaths.libsvm = '/home/adutta/Dropbox/Personal/Workspace/AdditionalTools/' ;
-    libpaths.vlfeat = '/home/adutta/Dropbox/Personal/Workspace/AdditionalTools/' ;
+    libpaths.matlabbgl = '' ;
+    libpaths.libsvm = '' ;
+    libpaths.vlfeat = '' ;
 
 end 
