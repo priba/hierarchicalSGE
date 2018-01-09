625935f97325dde41418992b4c87390ba559f94d
priba <priba@cvc.uab.es>
2018-01-09 12:16:15 +0100

    Extract statistics from results

---
+++ result_analisis/analyze_results.m
@@ -0,0 +1,9 @@
+function [ output_args ] = analyze_results( result_folder )
+%ANALYZE_RESULTS Summary of the results
+
+    % Generate summary file
+    generate_summary(result_folder)
+    
+    % Generate statistics
+end
+
---
+++ result_analisis/generate_statistics.m
@@ -0,0 +1,35 @@
+function [] = generate_statistics(result_folder , k)
+
+    if nargin < 2
+        k = 5 ;
+    end ; % if
+    
+    function print_statistics(T)
+        summary(T(:, {'Mean', 'Std'}))
+
+        [~ , ind] = sort(T.Mean,'descend') ;
+
+        fprintf('Top %d:', k);
+        T(ind(1:k),:)
+    end % function
+    
+    T = readtable( [ result_folder , 'summary.csv' ] ) ;
+    
+    try
+        T.Var11 = [];
+    catch
+        fprintf('No empty column');
+    end ; % try-catch
+    
+    % Whole Data
+    fprintf('Whole Data:');
+    print_statistics(T)
+    
+    split_ind = strcmpi(T.Labelled, 'label') ;
+    
+    fprintf('Unlabel:');
+    print_statistics(T(~split_ind,:))
+    fprintf('Label:');
+    print_statistics(T(split_ind,:))
+    
+end % function
