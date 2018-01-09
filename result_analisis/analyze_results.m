aec0b745daf03b2f479d3ee8b0e12144e1dfe628
priba <priba@cvc.uab.es>
2018-01-09 12:17:52 +0100

    Small fix

--- result_analisis/analyze_results.m
+++ result_analisis/analyze_results.m
@@ -1,9 +1,10 @@
-function [ output_args ] = analyze_results( result_folder )
+function [ ] = analyze_results( result_folder )
 %ANALYZE_RESULTS Summary of the results
 
     % Generate summary file
     generate_summary(result_folder)
     
     % Generate statistics
+    generate_statistics(result_folder, 5)
 end
 
