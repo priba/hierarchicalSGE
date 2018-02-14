3716429db163ceabd2adcc75377ef4f5f5f813ef
AnjanDutta <duttanjan@gmail.com>
2018-02-14 10:30:06 +0100

    small modifs in experimental settings

--- experiments.m
+++ experiments.m
@@ -6,27 +6,44 @@ function run_params = experiments()
 eps_i = [ 0.1 ; 0.05 ] ;
 del_i = [ 0.1 ; 0.05 ] ;
 max2 = [7; 5; 5];
-node_label = {'label'};
+node_label = {'unlabel', 'label'};
 node_label_id = (1:size(node_label,2))';
 
 % Hierarchy
-pyr_levels = [ 1 ; 2 ; 3 ] ;
+pyr_levels = [ 1; 2 ; 3 ] ;
 pyr_reduction = [ 1.0 ] ; % [ 2.0 ]
 delta = [ 0.0 ] ;
 clustering_func = { @girvan_newman };
 clustering_func_id = (1:size(clustering_func,2))';
-config = {'base', 'level', 'hier', 'comb'} ;
-config_id = (1:size(config,2))';
+config = {'level_pyr'} ;
+config_id = (1:size(config, 2))';
 
 % Embedding
 emb_comb = allcomb( { eps_i, del_i, node_label_id } );
 % emb_comb = [emb_comb,repmat(max2,size(emb_comb,1),1)];
 
 % Pyramid
-pyr_comb = allcomb( { pyr_levels, pyr_reduction , delta , clustering_func_id, config_id} );
-pyr_comb(pyr_comb(:,1) == 1,:) = [];
-pyr_comb(pyr_comb(:,5) == 1,:) = [];
-pyr_comb = [1, 1, 0, 1, 1; pyr_comb];
+pyr_comb = allcomb( { pyr_levels, pyr_reduction, delta, clustering_func_id, config_id } );
+
+flag1 = 0 ;
+idx = (pyr_levels == 1) ;
+if any(idx)
+    idx = find(idx) ;
+    pyr_comb(pyr_comb(:, 1) == idx, :) = [] ;
+    flag1 = 1 ;
+end
+
+flag2 = 0 ;
+idx = strcmp(config, 'base') ;
+if any(idx)
+    idx = find(idx) ;
+    pyr_comb(pyr_comb(:, 5) == idx, :) = [] ;
+    flag2 = 1 ;
+end
+
+if flag1 && flag2
+    pyr_comb = [1, 1, 0, 1, 1; pyr_comb];
+end
 
 ind_comb = allcomb({(1:size(emb_comb,1))' , (1:size(pyr_comb,1))'});
 
